18bdf15e3cd7946a280531a885973fec28ef60ff
AndrewE1234 <andrew@omnicode.com>
2020-03-07 01:26:21 -0800

    renamed math to math-problem and added ssu logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.css";
-import MathProblem from "./components/math";
+import MathProblem from "./components/math-problem";
 
 function App() {
   return (
     <div className="App">
+      <img
+        src="https://web.sonoma.edu/logos/logos/preview-sec-blue-box.jpg"
+        alt="SSU Logo"
+      />
       <h1>Math Quiz!</h1>
       <h3>Built with React, hosted on an Raspberry Pi 10 miles away!</h3>
       <MathProblem />
